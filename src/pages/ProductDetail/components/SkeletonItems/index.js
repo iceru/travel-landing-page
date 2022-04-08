bdda13e7740877474e9b0f11d8a5d81ef147ddd0
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-04-09 04:58:14 +0700

    fix skeleton items

--- src/pages/ProductDetail/components/SkeletonItems/index.js
+++ src/pages/ProductDetail/components/SkeletonItems/index.js
@@ -11,14 +11,14 @@ const SkeletonItems = ({ skeletonItemShow }) => {
     <div className="productSkeleton mb-4" style={{ display: skeletonItemShow }}>
       <Skeleton height={30} width="30%" className="mb-3" />
 
-      {[...Array(2)].map((data, i) => (
+      {[...Array(3)].map((data, i) => (
         <div key={i} id={data} className="row align-items-center mb-3">
-          <div className="col-4">
+          <div className="col-12 col-lg-4">
             <Skeleton height={20} width="50%" className="mb-2" />
             <Skeleton height={80} width="30%" className="mb-2" />
             <Skeleton height={15} count={3} width="70%" className="mb-2" />
           </div>
-          <div className="col-1 ms-auto">
+          <div className="col-6 col-lg-1 ms-auto">
             <Skeleton height={35} width="100%" />
           </div>
         </div>
