86afaa27a042c9b397c30810569e35fd39a20e7e
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-06-04 22:47:21 +0700

    update endpoints

--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -1,4 +1,4 @@
-const baseEndpoint = "https://devbook.txj.co.jp/v4";
+const baseEndpoint = "https://book.txj.co.jp/v4";
 
 const serviceEndpoint = `${baseEndpoint}/Services`;
 const pageEndpoint = `${baseEndpoint}/Pages`;
